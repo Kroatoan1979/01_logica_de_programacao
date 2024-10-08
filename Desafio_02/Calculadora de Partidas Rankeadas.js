a08d99c1e450347b70da5346728b575df1284d71
Ivan Almeida <ivan.almeida1979@gmail.com>
2024-10-08 14:45:10 +0100

    08/10/2024 14:45 - Desafio_02

--- Desafio_02/Calculadora de Partidas Rankeadas.js
+++ Desafio_02/Calculadora de Partidas Rankeadas.js
@@ -24,3 +24,28 @@
 
 // Ao final deve se exibir uma mensagem:
 // "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
+
+function calculadora(vitorias, derrotas){
+    saldo = vitorias - derrotas
+    return saldo
+}
+
+let resultado = calculadora(72, 45)
+
+if (resultado < 10){
+    nivel = "Ferro"
+} else if (resultado >= 11 && resultado < 20){
+    nivel = "Bronze"
+}else if (resultado >= 21 && resultado < 50){
+    nivel = "Prata"
+}else if (resultado >= 51 && resultado < 80){
+    nivel = "Ouro"
+}else if (resultado >= 81 && resultado < 90){
+    nivel = "Diamante"
+}else if (resultado >= 91 && resultado < 100){
+    nivel = "Lendário"
+}else if (resultado >= 101){
+    nivel = "Imortal"
+}
+
+console.log(`O Herói tem saldo de **${resultado}** está no nível de **${nivel}**`)
