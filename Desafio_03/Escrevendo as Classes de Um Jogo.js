78fd3b7fce819999bda73bbc030ca81682e5e2db
Ivan Almeida <ivan.almeida1979@gmail.com>
2024-10-10 15:43:49 +0100

    10/10/2024 15:43 - Desafio_03

---
+++ Desafio_03/Escrevendo as Classes de Um Jogo.js
@@ -0,0 +1,42 @@
+class criarHeroi{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    
+    atacar(){
+        let ataque
+
+        switch(this.tipo){
+            case "mago":
+                ataque = "magia"
+                break
+            case "guerreiro":
+                ataque = "espada"
+                break
+            case "monge":
+                ataque = "artes marciais"
+                break
+            case "ninja":
+                ataque = "shuriken"
+                break
+            default:
+                ataque = "ataque desconhecido"
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataque}`)
+
+    }
+
+}
+
+heroi = new criarHeroi("Gandalf", 100, "mago")
+heroi.atacar()
+heroi = new criarHeroi("Gandalf", 100, "guerreiro")
+heroi.atacar()
+heroi = new criarHeroi("Gandalf", 100, "monge")
+heroi.atacar()
+heroi = new criarHeroi("Gandalf", 100, "ninja")
+heroi.atacar()
+heroi = new criarHeroi("Gandalf", 100, "Super Guerreiro")
+heroi.atacar()
